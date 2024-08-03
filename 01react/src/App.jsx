5300c741fdf40305c9ac3f39284dc06cd52afa2f
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-04 02:53:37 +0530

    basics react

--- 01react/src/App.jsx
+++ 01react/src/App.jsx
@@ -1,14 +1,17 @@
-import Chai from "./one.jsx";
+// eslint-disable-next-line no-unused-vars
+import Chai from "./One.jsx";
 
 function App() {
   const username = "chai aur react";
   // app is function jo html ko return krta hai & isko render karwa do gye
   return <h1>Chai {username}</h1>;
 }
-// INTERVIEW : {username} isko bolte hai evaluted expression { javascript ka final outcome ko likhte hai yaha}
-// jo  {} kai andar h usko var ki tarah treat krengye
+// INTERVIEW : {username} isko bolte hai evaluted expression (yaha if else nhi likh skte ){ javascript ka final outcome ko likhte hai yaha}
+// jo  {iske} kai andar h usko varaible ki tarah treat krengye
 
-/* must be wrapeed in enclosing tag  ( 1 element return uske andar bohot jada)
+// BASIC INTROO
+/* JSX KA RULE HAI EK HE ELEMENT RETURN KAR SKTE H
+ must be wrapeed in enclosing tag  ( 1 element return uske andar bohot jada)
 <div>
   <h1>fdbd</h1>
   <p>dfbs</p>
--- 01react/src/main.jsx
+++ 01react/src/main.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// eslint-disable-next-line no-unused-vars
 import App from "./App.jsx";
+
+// eslint-disable-next-line no-unused-vars
 function MyApp() {
   //1
   return (
     <div>
       <h1> Custom App</h1>
     </div>
-  ); // jo yaha likha hai vo convert hota h reactelement mai
+  ); // jo yaha likha hai vo convert hota h react element mai jo hmne custom react mai padha h
 }
-//4
+ReactDOM.createRoot(document.getElementById("root")).render(<MyApp />);
+//(<MyApp />); // or write MyApp() ese bhi run hoga
+
+//3
+// eslint-disable-next-line no-unused-vars
 const anotherElement = (
   <a href="https://google.com" target="_blank">
     VISIT GOOGLE
   </a>
 );
+ReactDOM.createRoot(document.getElementById("root")).render(anotherElement);
+// isko <another element /> ESE NHI LIKH SKTE
 
-//5 I WANT TO INJECT VARAIBLE
-const a = "dgbd";
-
-/* YEH HAM KHUD SAI KAR RHE THAI ISLIYE PRINT NHI HOO RHA THA 
+/* YEH HAM KHUD SAI KAR RHE THAI ISLIYE PRINT NHI HOO RHA THA  
 YEH HMARA WALA METHOD HAI  //2
 
 const reactElement = {
@@ -32,17 +38,21 @@ const reactElement = {
   },
   children: "Click on me ",
 };
+ReactDOM.createRoot(document.getElementById("root")).render(
+<ReactElement /> // YEH RENDER TOH REACT NE LIKHA H BUT 
+);
 */
 
-// abh react kai hisab sai bnao element KHUD KAI NHI //3
+//5 I WANT TO INJECT VARAIBLE
+const aaaaaaaa = "dgbd";
+
+// abh react kai hisab sai bnao element KHUD KAI NHI //4
 const reactelement = React.createElement(
-  // phele tag , fir object
+  // phele tag , fir object(prop add krta hai)
   "a",
   { href: "https://google.com", target: "_blank" },
   "VISIT GOOGLE noww", // jo text inject krna h
-  a // another var inject hogya
+  aaaaaaaa // another var inject hogya
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(reactelement);
-//.render(anotherElement);
-// isko <another element /> ESE NHI LIKH SKTE //(<MyApp />); // or write MyApp() ese bhi run hoga
