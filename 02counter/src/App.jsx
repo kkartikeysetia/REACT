4215a9e019b3286511585ab04d922fff8230cad4
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-04 17:59:59 +0530

    counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,23 +1,50 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 // yaha react bina import kare hua hai babble sai
 function App() {
-  // first hook YEH UPDATE HOTI HAI JAISE NICHE UPDATE KARI HAI JAGAH JAGAH
+  // use state is responsible for  change it does not mean value change : iss change ko propgate kai jaata h ui kai andar (dom kai andar)
+  // KUCH HOOKS KAI THROUGH DATA UPDATE HO RHA first hook YEH UPDATE HOTI HAI JAISE NICHE UPDATE KARI HAI JAGAH JAGAH
+  // use state (andar kuch bhi default value doo ('',{},[] ) )
+  // use state sai do cheez milti h aaray kai format mai (varaible ,function)
+
   let [counter, setcounter] = useState(15); // isko counter ++ nhi likhna yeh apne app update hoga
-  // setcounter is fun jo counter wale ko update krta hai
+  // setcounter is function ya method jo counter wale ko update krta hai (counter is just var)
+  // abh react apne aap react krega jaha jaha counter hoga apne aap update hoga har jagah APNE AAP STATE UPDATE HO JAYE GYI
 
   // let counter = 10; // koi value inject krni h toh
 
   const addvalue = () => {
-    console.log("clicked", counter);
-    // counter++;
-    counter = counter + 1;
-    setcounter(counter); // OR setcounter(counter+1)
+    // console.log("clicked", counter);
+    if (counter < 20) {
+      setcounter(counter + 1);
+    }
   };
-  /* INTERVIEW QUESTION
+  // counter = counter + 1;
+  // setcounter(counter); // OR setcounter(counter+1)
+  // phele value update hogyi then set counter mai aa gyi react andar sai raect kara value trigger huyi jaha jaha counter tha waha update kardi
+
+  const removevalue = () => {
+    if (counter > 0) {
+      setcounter(counter - 1);
+    }
+  };
+
+  return (
+    <>
+      <h1>project 02 </h1>
+      <h2>counter value :{counter}</h2>
+      <button onClick={addvalue}>Add value {counter}</button>
+      <br></br>
+      <button onClick={removevalue}>remove value {counter}</button>
+      <p> fotter:{counter}</p>
+    </>
+  );
+}
+
+export default App;
+/* INTERVIEW QUESTION
   const addvalue=()=>{
     setcounter(prevCounter=> prevCounter+1)
     setcounter(prevCounter=> prevCounter+1)
@@ -25,20 +52,3 @@ function App() {
     setcounter(prevCounter=> prevCounter+1)
   }
     */
-
-  const removevalue = () => {
-    setcounter(counter - 1);
-  };
-
-  return (
-    <>
-      <h1>project 02 {counter}</h1>
-      <h2>counter value :{counter}</h2>
-      <button onClick={addvalue}>Add value {counter}</button>
-      <br></br>
-      <button onClick={removevalue}>remove value {counter}</button>
-    </>
-  );
-}
-
-export default App;
--- 02counter/src/main.jsx
+++ 02counter/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+// yaha app load ho rha h
+// REACT : ek button per click krne pe 5 jagah updates (react krti h var kai updation)
