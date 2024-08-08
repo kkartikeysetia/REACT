acf2bba2b3b3ff552641ef7584df0ce135b904e1
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-08 21:13:22 +0530

    signup/login

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -1,10 +1,21 @@
-// eslint-disable-next-line no-unused-vars
-import { useState } from "react";
-
 import "./App.css";
+import "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import Toggle from "./components/Toggle";
+import Login from "./components/Login";
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Toggle />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/dashboard" element={<div>Dashboard Page</div>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
