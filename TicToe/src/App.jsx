fc1bce02f93fff84e2a7e428b3e8bff34a509037
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-09 22:13:53 +0530

    ticc toe

---
+++ TicToe/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Board from "./components/Board";
+import Square from "./components/Square";
+
+function App() {
+  return (
+    <>
+      <Board />
+    </>
+  );
+}
+
+export default App;
