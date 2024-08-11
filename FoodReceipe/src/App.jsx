c540d433852e602cf089e61726b884dd32d11e5b
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-11 22:52:07 +0530

    app .jsx food

--- FoodReceipe/src/App.jsx
+++ FoodReceipe/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Detail from "./components/Detail";
+import Home from "./components/Home";
+import Fav from "./components/Fav";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div>
+      <Navbar />
+      {/* to swicth from one page to another */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fav" element={<Fav />} />
+        {/* for each food item is differnet  */}
+        <Route path="/recipe-item/:id" element={<Detail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
