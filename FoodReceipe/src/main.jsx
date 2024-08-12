d7b6addc0240013f099109b075ab4318e2359dde
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-12 16:34:21 +0530

    main .jsx

--- FoodReceipe/src/main.jsx
+++ FoodReceipe/src/main.jsx
@@ -4,10 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+import GlobalState from "./context/Context.jsx";
+
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
-      <App />
+      <GlobalState>
+        <App />
+      </GlobalState>
     </StrictMode>
   </BrowserRouter>
 );
