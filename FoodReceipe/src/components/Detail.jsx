4599ac5d56dc491a8cef675dad126a74e4ae4a40
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-12 22:23:24 +0530

    deatils

--- FoodReceipe/src/components/Detail.jsx
+++ FoodReceipe/src/components/Detail.jsx
@@ -1,3 +1,42 @@
+// once you  go on deatil page based on id call api the render details
+// we need to extract id from url : import hook from params
+
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { GlobalContext } from "../context/Context";
+
 export default function Detail() {
-  return <div>Detail</div>;
+  const Params = useParams();
+  const { recipeDetailsData, setRecipeDetailsData } = useContext(GlobalContext);
+  {
+    /* getRecipeDetails is an asynchronous function that fetches data from an external API 
+    using the recipe ID from the URL parameters (id). */
+  }
+  useEffect(() => {
+    async function getRecipeDetails() {
+      const response = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      );
+      const data = await response.json();
+
+      console.log(data);
+      if (data?.data) {
+        setRecipeDetailsData(data?.data);
+      }
+    }
+    getRecipeDetails();
+  }, []);
+
+  return (
+    <div className="container mx-auto py-10 grid grid-cols-1 lg:grid-cols-2 gap-10">
+      <div className="row-start-2 lg:row-start-auto">
+        <div className="h-96 overflow-hidden rounded-xl group">
+          <img
+            src={recipeDetailsData?.recipe?.image_url}
+            className="w-full h-full object-cover block group-hover:scale-105 duration-300"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
