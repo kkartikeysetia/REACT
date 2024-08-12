1cb1113dbe2bde62ee4e4de025f556f69f8f048a
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-12 19:08:23 +0530

    navbar

--- FoodReceipe/src/components/Navbar.jsx
+++ FoodReceipe/src/components/Navbar.jsx
@@ -3,21 +3,23 @@ import { NavLink } from "react-router-dom";
 import { GlobalContext } from "../context/Context";
 
 export default function Navbar() {
-  //accepsts the context object
-  const { search, setSearch } = useContext(GlobalContext);
+  //accepsts the context object & return currenty context value
+  const { search, setSearch, handleSubmit } = useContext(GlobalContext);
+  // means from global context using this hook we are accessing search & set Search
   return (
     <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
       <h2 className="text-2xl font-semibold">
         <NavLink to={"/"}></NavLink>
         Food Recipe
       </h2>
-      <form {handleSubmit}>
+      {/* on submit of this form  get search value & based on that call api */}
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter items"
           name="search"
           value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)} // iske baad handlesubmit becoz click per krne per call api
           className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-500"
         />
       </form>
