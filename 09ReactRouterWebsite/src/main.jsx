6035a197f77dc20f78812e0a02adf981218214fc
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-13 16:18:10 +0530

    MAIN

--- 09ReactRouterWebsite/src/main.jsx
+++ 09ReactRouterWebsite/src/main.jsx
@@ -2,13 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
 
 // Router bnao
 // yeh ek method hai isk anadr array haui  iske andar sare list daal do jo obejct chahiye
-const router = createBrowserRouter([
+/*const router = createBrowserRouter([
+
   {
     path: "/",
     element: <App />,
@@ -21,9 +28,25 @@ const router = createBrowserRouter([
         path: "about",
         element: <About />,
       },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
     ],
   },
 ]);
+*/
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+    </Route>
+  )
+);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
