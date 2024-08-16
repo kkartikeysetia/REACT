3cf389e20487a4bc4101bc62f260bcf5ef21c798
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-16 18:11:47 +0530

    user

--- 07MiniContext/src/context/UserContext.js
+++ 07MiniContext/src/context/UserContext.js
@@ -6,7 +6,7 @@ export default UserContext;
 /* context kia sath provider bhi bnna hota hai jo data provide krta h
 usercontext is a provider iske sath ek provider bhi bnana hota h jo data provide krta h
 componensts bnaye wrap kiya usercontext sai toh yeh ban gya : provider meeans
- iske andar jo bhi componensts unko access milgya global context ka & sari states ka through user context
+iske andar jo bhi componensts unko access milgya global context ka & sari states ka through user context
 
 <usercontext>
 <login></login>
