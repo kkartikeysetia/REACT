091f530eef58986464ce9e8ed1cc0063f12084cd
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-19 00:11:28 +0530

    login

--- 07MiniContext/src/components/login.jsx
+++ 07MiniContext/src/components/login.jsx
@@ -40,5 +40,5 @@ function Login() {
   );
 }
 export default Login;
-// main thing jo usercontext kai andar value h usko kaise fetch krna h
+// Main thing jo usercontext kai andar value h usko kaise fetch krna h
 // Use state ki tarah he hook h
