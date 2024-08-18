5b990a621c866867a0464e5e4e51915732f6918f
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-18 23:06:01 +0530

    user context

--- 07MiniContext/src/context/UserContext.js
+++ 07MiniContext/src/context/UserContext.js
@@ -3,7 +3,7 @@ import React from "react";
 const UserContext = React.createContext();
 export default UserContext;
 
-/* context kia sath provider bhi bnna hota hai jo data provide krta h
+/*Context kia sath provider bhi bnna hota hai jo data provide krta h
 usercontext is a provider iske sath ek provider bhi bnana hota h jo data provide krta h
 componensts bnaye wrap kiya usercontext sai toh yeh ban gya : provider meeans
 iske andar jo bhi componensts unko access milgya global context ka & sari states ka through user context
