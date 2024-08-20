203ae9c2588733e25f5ca7c758ec4248b058bc07
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-20 23:49:38 +0530

    theme

--- 08ThemeSwitcher/src/context/theme.js
+++ 08ThemeSwitcher/src/context/theme.js
@@ -2,7 +2,7 @@ import{useContext, createContext,useState}from 'react'; // waha crate context bn
 
 export const ThemeContext=createContext({//pichle mai () ismai kuch nhi lia tha
 //Jab context bane koi value feed ho phele sai hee 
-//Yaha mai chahta hun koi object default value ho becoz directly koi call krdega toh crash hojayega 
+//Yaha maii chahta hun koi object default value ho becoz directly koi call krdega toh crash hojayega 
     themeMode:"light"
     // just 2 methods 
     darkTheme:()=>{}
