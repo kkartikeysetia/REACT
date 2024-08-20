59f020da4d3aa5323d35f2a32df9988c90364e0a
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2024-08-20 23:50:36 +0530

    index

--- ToDoLocalStorage/src/Components/index.js
+++ ToDoLocalStorage/src/Components/index.js
@@ -1,3 +1,3 @@
-//Folder kai andar index file islye tanki sara import yaaha lele
+//Folder kai andar index file islye tanki sara import yaha lele
 export { TodoContext, useTodo, TodoProvider } from "./Context";
 //Use todo sai use context sai todocontext
